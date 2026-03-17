5732db59c8e0799326d458af85c3ba0b6ddcc5a1
Aju <ajilaries20@gmail.com>
2026-03-17 19:26:51 +0530

    app theme

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class AppTheme {
+  static ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: AppColors.background,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+  );
+}
