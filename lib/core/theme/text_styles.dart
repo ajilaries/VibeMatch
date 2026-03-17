0770e1b606f0aa086a54c1cfd230ed20ab2bd7fc
Aju <ajilaries20@gmail.com>
2026-03-17 19:27:26 +0530

    text style

---
+++ lib/core/theme/text_styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+class AppTextStyles {
+  static const heading = TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+    color: AppColors.textPrimary,
+  );
+  static const Title = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: AppColors.textPrimary,
+  );
+  static const body = TextStyle(fontSize: 16, color: AppColors.textSecondary);
+}
