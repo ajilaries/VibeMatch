d4484dad72424f83ee3c8f58d946810078130cc3
Aju <ajilaries20@gmail.com>
2026-03-17 19:25:55 +0530

    chat screen

---
+++ lib/screens/chat/chat_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Chats"),
+      ),
+
+      body: const Center(
+        child: Text(
+          "Your Messages 💬",
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
