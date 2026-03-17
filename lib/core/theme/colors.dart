4c84af8127e296369019333819a22fac13942040
Aju <ajilaries20@gmail.com>
2026-03-17 19:27:07 +0530

    colors screen

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const primary = Color(0xFF6C63FF);
+
+  static const background = Color(0xFF0F0F1A);
+
+  static const card = Color(0xFF1C1C2E);
+
+  static const textPrimary = Colors.white;
+
+  static const textSecondary = Colors.grey;
+}
