5e17cade4efa28f0c3433e248da838a617d8e9d9
Aju <ajilaries20@gmail.com>
2026-03-18 09:15:29 +0530

    chat screen

--- lib/screens/chat/chat_screen.dart
+++ lib/screens/chat/chat_screen.dart
@@ -1,21 +1,74 @@
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+
+  final TextEditingController controller = TextEditingController();
+
+  List<String> messages = [];
+
+  void sendMessage() {
+    if (controller.text.isEmpty) return;
+
+    setState(() {
+      messages.add(controller.text);
+      controller.clear();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Chats"),
-      ),
+      appBar: AppBar(title: const Text("Chat")),
 
-      body: const Center(
-        child: Text(
-          "Your Messages 💬",
-          style: TextStyle(fontSize: 22),
-        ),
+      body: Column(
+        children: [
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+
+                return ListTile(
+                  title: Align(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      color: Colors.blue,
+                      child: Text(messages[index]),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+
+          Row(
+            children: [
+
+              Expanded(
+                child: TextField(
+                  controller: controller,
+                  decoration: const InputDecoration(
+                    hintText: "Type message...",
+                  ),
+                ),
+              ),
+
+              IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: sendMessage,
+              )
+            ],
+          )
+        ],
       ),
     );
   }
