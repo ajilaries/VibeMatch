f3b369c95a286efb8dc71842e36caa7e2a51edce
Aju <ajilaries20@gmail.com>
2026-03-18 09:16:06 +0530

    main navigation

---
+++ lib/features/navigation/main_navigation.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:vibematch/features/messages/message_screen.dart';
+import '/screens/home/home_screen.dart';
+import '/screens/home/discover_screen.dart';
+import '/screens/profile/profile_screen.dart';
+import '/screens/settings/settings_screen.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int currentIndex = 0;
+  final List<Widget> screens = [
+    DiscoverScreen(),
+    HomeScreen(),
+    MessagesScreen(),
+    ProfileScreen(),
+    SettingsScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[currentIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.explore), label: "Discover"),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chat"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
+      ),
+    );
+  }
+}
