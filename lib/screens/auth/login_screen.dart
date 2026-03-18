128926a6001bf08e3fbb8ec39d80e614c63858aa
Aju <ajilaries20@gmail.com>
2026-03-18 09:15:48 +0530

    login screen

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:vibematch/screens/home/home_screen.dart';
+import '../../features/navigation/main_navigation.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -23,7 +23,7 @@ class LoginScreen extends StatelessWidget {
               onPressed: () {
                 Navigator.pushReplacement(
                   context,
-                  MaterialPageRoute(builder: (context) => const HomeScreen()),
+                  MaterialPageRoute(builder: (context) => const MainNavigation()),
                 );
               },
               child: const Text("Login"),
