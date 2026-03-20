ac93c4980fd19213872a0f528cc292be3c0e74d7
Aju <ajilaries20@gmail.com>
2026-03-20 12:57:37 +0530

    main navigation controller

--- lib/features/navigation/main_navigation.dart
+++ lib/features/navigation/main_navigation.dart
@@ -17,7 +17,7 @@ class _MainNavigationState extends State<MainNavigation> {
   final List<Widget> screens = [
     DiscoverScreen(),
     HomeScreen(),
-    MessagesScreen(),
+    MessageScreen(),
     ProfileScreen(),
     SettingsScreen(),
   ];
