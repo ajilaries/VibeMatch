8f040da12834cfbbbc9b9f096b84e851e12b3918
Aju <ajilaries20@gmail.com>
2026-03-20 12:54:40 +0530

    chat ui

--- lib/screens/chat/chat_screen.dart
+++ lib/screens/chat/chat_screen.dart
@@ -1,75 +1,82 @@
 import 'package:flutter/material.dart';
+import '../../core/services/chat_service.dart';
 
 class ChatScreen extends StatefulWidget {
-  const ChatScreen({super.key});
+  final int userId;
+  final int receiverId;
+
+  const ChatScreen({super.key, required this.userId, required this.receiverId});
 
   @override
   State<ChatScreen> createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
-  final TextEditingController controller = TextEditingController();
+  final TextEditingController messageController = TextEditingController();
 
   List<String> messages = [];
 
+  @override
+  void initState() {
+    super.initState();
+
+    ChatService.connect(widget.userId);
+
+    ChatService.channel.stream.listen((data) {
+      setState(() {
+        messages.add(data.toString());
+      });
+    });
+  }
+
   void sendMessage() {
-    if (controller.text.isEmpty) return;
+    if (messageController.text.isEmpty) return;
+
+    ChatService.sendMessage(widget.receiverId, messageController.text);
 
     setState(() {
-      messages.add(controller.text);
-      controller.clear();
+      messages.add("Me:${messageController.text}");
     });
+    messageController.clear();
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(title: const Text("Chat")),
 
       body: Column(
         children: [
-
           Expanded(
             child: ListView.builder(
               itemCount: messages.length,
               itemBuilder: (context, index) {
-
-                return ListTile(
-                  title: Align(
-                    alignment: Alignment.centerRight,
-                    child: Container(
-                      padding: const EdgeInsets.all(10),
-                      color: Colors.blue,
-                      child: Text(messages[index]),
-                    ),
-                  ),
-                );
+                return ListTile(title: Text(messages[index]));
               },
             ),
           ),
 
           Row(
             children: [
-
               Expanded(
                 child: TextField(
-                  controller: controller,
-                  decoration: const InputDecoration(
-                    hintText: "Type message...",
-                  ),
+                  controller: messageController,
+                  decoration: const InputDecoration(hintText: "Type a message"),
                 ),
               ),
 
-              IconButton(
-                icon: const Icon(Icons.send),
-                onPressed: sendMessage,
-              )
+              IconButton(icon: const Icon(Icons.send), onPressed: sendMessage),
             ],
-          )
+          ),
         ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    ChatService.channel.sink.close();
+    messageController.dispose();
+    super.dispose();
   }
 }
