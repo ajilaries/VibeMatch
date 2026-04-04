189fcb5e6e9e72c9da9e896e732463bed6fa6634
Aju <ajilaries20@gmail.com>
2026-04-04 20:02:33 +0530

    snap model has upgraded with id for snaps

--- lib/models/snap_model.dart
+++ lib/models/snap_model.dart
@@ -1,13 +1,23 @@
 class SnapModel {
+  final String id;
   final String imagePath;
   final String username;
   final DateTime timestamp;
-  final String type;//image or video
+  final String type; // image or video
+
+  // ✅ ADD THIS
+  final int viewDuration;
+
+  // ✅ (for later use - seen count)
+  final List<dynamic> viewedBy;
 
   SnapModel({
+    required this.id,
     required this.imagePath,
     required this.username,
     required this.timestamp,
     required this.type,
+    this.viewDuration = 5, // default 5 sec
+    this.viewedBy = const [],
   });
 }
