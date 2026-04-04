1a3cee7225b07ad067ab8eb97605edb0eac8340a
Aju <ajilaries20@gmail.com>
2026-04-04 20:03:09 +0530

    function defined for snap view mark

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -137,16 +137,31 @@ class ApiService {
       throw Exception("Upload failed");
     }
   }
-  static Future<void> removeProfileMedia(String token) async {
-  final response = await http.delete(
-    Uri.parse('$baseUrl/profile/media'),
-    headers: {
-      'Authorization': 'Bearer $token',
-    },
-  );
 
-  if (response.statusCode != 200) {
-    throw Exception("Remove failed");
+  static Future<void> removeProfileMedia(String token) async {
+    final response = await http.delete(
+      Uri.parse('$baseUrl/profile/media'),
+      headers: {'Authorization': 'Bearer $token'},
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception("Remove failed");
+    }
   }
-}
+
+  static Future<void> markSnapViewed({
+    required String snapId,
+    required String token,
+  }) async {
+    final response = await http.post(
+      Uri.parse("&baseUrl/snaps/view/$snapId"),
+      headers: {
+        "Authorization": "Bearer $token",
+        "Content-Type": "application/json",
+      },
+    );
+    if (response.statusCode != 200) {
+      throw Exception("Failed to mark snap as viewed");
+    }
+  }
 }
